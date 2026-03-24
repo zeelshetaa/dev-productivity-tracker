da0c3d3c66bd797f97feba5b14dadb7ead447757
zeelshetaa <zeelsheta1958@gmail.com>
2026-03-24 10:10:26 +0530

    Update report_generator.py

--- dev-productivity-tracker/report_generator.py
+++ dev-productivity-tracker/report_generator.py
@@ -1,19 +1,27 @@
 from datetime import datetime
 
-def print_console_report(data):
 
-    print("\nDeveloper Productivity Report")
-    print("-----------------------------------")
+def generate_report(data, score, level, insights, recommendations):
 
-    print(f"Report Generated : {datetime.now()}")
-    print(f"Total Commits    : {data['commits']}")
-    print(f"Lines Added      : {data['lines_added']}")
-    print(f"Lines Removed    : {data['lines_removed']}")
+    print("\n===================================")
+    print("AI Developer Productivity Report")
+    print("===================================")
 
-    net = data["lines_added"] - data["lines_removed"]
+    print(f"Time                : {datetime.now()}")
+    print(f"Total Commits       : {data['commits']}")
+    print(f"Files Changed       : {data['files_changed']}")
+    print(f"Lines Added         : {data['lines_added']}")
+    print(f"Lines Removed       : {data['lines_removed']}")
 
-    print(f"Net Code Change  : {net}")
-    print(f"Estimated Coding Hours : {data['hours']}")
-    print(f"Activity Level   : {data['activity_level']}")
+    print(f"\nProductivity Score  : {score}")
+    print(f"Productivity Level  : {level}")
 
-    print("-----------------------------------\n")
+    print("\nInsights:")
+    for k, v in insights.items():
+        print(f"- {k}: {v}")
+
+    print("\nAI Recommendations:")
+    for rec in recommendations:
+        print(f"- {rec}")
+
+    print("===================================\n")
